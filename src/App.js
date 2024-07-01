d4aae6ee872cfad4f6b59dcd1c872c6c1682b3c5
KandruPoojitha <poojithakandru.ed@gmail.com>
2024-06-30 20:40:12 -0400

    Forgot password, Register js files and few other changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
-// import Home from './components/Home';
-import UploadBook from './components/UploadBook';
+import Register from './components/Register';
+import Home from './components/Home'
 
 function App() {
+  
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<UploadBook />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
     </Router>
